1f5907b3e5769c2602a66d460315b4a0fa4113ad
Carlos Delbarre <cadelbarre@gmail.com>
2021-04-30 17:24:58 -0500

    Adicionado recuperar contraseña firebase

--- src/components/Login/ResetPasswordUser.vue
+++ src/components/Login/ResetPasswordUser.vue
@@ -10,24 +10,41 @@
             Ingrese su dirección de correo electrónico y le enviaremos un enlace para restablecer su contraseña.
         </p>
         <b-field grouped group-multiline position="is-centered">
-            <b-input ref="resetPass" placeholder="Ingrese su correo electrónico." expanded></b-input>
+            <b-input ref="resetPass" placeholder="Ingrese su correo electrónico." expanded v-model="email" :type="email != null ? 'is-danger' : ''"></b-input>
             <p class="control">
-                <b-button label="Restaurar Contraseña" type="is-info" expanded></b-button>
+                <b-button label="Restaurar Contraseña" type="is-info" expanded @click="recoveryPassword(email)"></b-button>
             </p>
         </b-field>
         <hr>
         <p class="has-text-centered mb-5">
             ¿ Estas listo para iniciar sesión ? Entonces, <router-link to="/">vamos alla</router-link>
         </p>
-
     </div>
 </template>
 <script>
+import Auth from "@/classes/AuthUser";
+import Toast from "@/classes/Toast";
+
 export default {
     name: 'resetPassword',
+    data() {
+        return {
+            email: null
+        }
+    },
     mounted() {
         this.$refs.resetPass.focus()
     },
+    methods: {
+        recoveryPassword(email) {
+            Auth.recoveryPassword(email).then((response)=>{
+                Toast.success(`Se envió un correo electrónico de restablecimiento de contraseña.`)
+            }).catch((e)=>{
+                Toast.error(`${e.code} - ${e.message}`)
+            })
+        }
+    }
+
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->

9b44e7ff69f6d738f596b8829aef5d0f91a007cb
Carlos Delbarre <cadelbarre@gmail.com>
2021-07-01 15:11:32 -0500

    Formatear numero ya lo muestra en positivo y negativo

--- src/functions/general.js
+++ src/functions/general.js
@@ -1,20 +1,15 @@
-const formattedNumber = (number = 0) => 
-    number.toFixed()
-    .toString()
-    .replace(/\D/g, "")
-    .replace(/\B(?=(\d{3})+(?!\d))/g, "."); 
+const formattedNumber = ( number = 0 ) => {
+    var fixed = number.toFixed()
+        .toString()
+        .replace( /\D/g, "" )
+        .replace( /\B(?=(\d{3})+(?!\d))/g, "." );
+    
+    var final = ''
+    number < 0 ? final = '- ' + fixed : final = fixed
 
-  //   const formattedNumber = (number = 0) => { 
-  //   var fixed = number.toFixed()
-  //   .toString()
-  //   .replace(/\D/g, "")
-  //   .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return final
 
-  //   var final = ''
-   // number < 0 ? final = '-'+fixed : final = fixed
-
-  //   return final 
-  // } 
+}
 
 export {
     formattedNumber
